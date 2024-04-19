b13d0209cd7a3f9652fb2ea329d9580590675057
Tran Nguyen Dang Huy <trannguyendanhuy2904@gmail.com>
2024-04-19 13:24:22 +0700

    base

--- src/components/experience-card.tsx
+++ src/components/experience-card.tsx
@@ -1,8 +1,15 @@
-import { Reveal } from "./reveal";
-import { Badge } from "./ui/badge";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Reveal } from './reveal';
+import { Badge } from './ui/badge';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from './ui/card';
 
 interface ExperienceCardProps {
+    isBlurred?: boolean;
     title: string;
     company: string;
     date: string;
@@ -10,22 +17,44 @@ interface ExperienceCardProps {
     skills?: string[];
 }
 
-export function ExperienceCard({ title, company, date, description, skills }: ExperienceCardProps) {
+export function ExperienceCard({
+    isBlurred,
+    title,
+    company,
+    date,
+    description,
+    skills,
+}: ExperienceCardProps) {
     return (
-        <Card className="w-full">
+        <Card className="w-full bg-gradient-to-b from-[rgb(20,15,20)] to-black shadow-inner shadow-gray-400 ">
             <Reveal
                 initial={{ opacity: 0, y: -50 }}
-                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.4 } }}
+                whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 0.4 },
+                }}
             >
                 <CardHeader>
-                    <CardTitle>{title} · {company}</CardTitle>
+                    <div className="flex py-2 gap-1 items-center">
+                        <span className="bg-red-500 size-3 rounded-full"></span>
+                        <span className="bg-yellow-500  size-3 rounded-full"></span>
+                        <span className="bg-green-500   size-3 rounded-full"></span>
+                    </div>
+                    <CardTitle>
+                        {title} · {company}
+                    </CardTitle>
                     <CardDescription>{date}</CardDescription>
                 </CardHeader>
             </Reveal>
             <CardContent className="flex flex-col gap-4">
                 <Reveal
                     initial={{ opacity: 0, x: -50 }}
-                    whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4 } }}
+                    whileInView={{
+                        opacity: 1,
+                        x: 0,
+                        transition: { duration: 0.4 },
+                    }}
                 >
                     <p className="leading-7">{description}</p>
                 </Reveal>
@@ -35,7 +64,14 @@ export function ExperienceCard({ title, company, date, description, skills }: Ex
                             <Reveal
                                 key={skill}
                                 initial={{ opacity: 0, y: 30 }}
-                                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.1, delay: index / 40 } }}
+                                whileInView={{
+                                    opacity: 1,
+                                    y: 0,
+                                    transition: {
+                                        duration: 0.1,
+                                        delay: index / 40,
+                                    },
+                                }}
                             >
                                 <Badge variant="default">{skill}</Badge>
                             </Reveal>
@@ -44,5 +80,5 @@ export function ExperienceCard({ title, company, date, description, skills }: Ex
                 )}
             </CardContent>
         </Card>
-    )
+    );
 }
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,57 +1,78 @@
-"use client";
+'use client';
 
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import logo from '@/lib/img/logo.png';
 import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import {
-    Sheet,
-    SheetContent,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Menu } from 'lucide-react';
+import Image from 'next/image';
 
 export function NavigationBar() {
     return (
         <>
-            <NavigationMenu className="hidden sm:flex sm:sticky sm:top-0 min-w-full bg-black bg-opacity-20 backdrop-blur-lg container py-4 justify-between border-b border-border/40">
-                <Link href="/" passHref>
-                    <Avatar className="cursor-pointer">
-                        <AvatarImage src="https://github.com/KeluhingBavui.png" alt="@KeluhingBavui" />
-                        <AvatarFallback>DW</AvatarFallback>
-                    </Avatar>
-                </Link>
+            <NavigationMenu className="hidden sm:flex sm:sticky sm:top-0 min-w-full bg-[#0a0a0a] bg-opacity-20 backdrop-blur-lg container py-4 justify-between border-b border-border/40">
+                <div className="flex justify-center items-center">
+                    <Link href="/" passHref>
+                        <Avatar className="cursor-pointer">
+                            <AvatarImage
+                                src={logo.src}
+                                alt="@EthanDrinkinCoffee"
+                            />
+
+                            <AvatarFallback>ET</AvatarFallback>
+                        </Avatar>
+                    </Link>
+                </div>
+
                 <NavigationMenuList className="w-full justify-end">
                     <NavigationMenuItem>
                         <Link href="#about" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
                                 About
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <Link href="#experiences" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
                                 Experiences
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <Link href="#projects" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
                                 Projects
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <Link href="#skills" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
                                 Skills
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <Link href="#contact" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
                                 Contact
                             </NavigationMenuLink>
                         </Link>
@@ -67,42 +88,58 @@ export function NavigationBar() {
                         <NavigationMenuList className="w-full justify-end flex-col pt-4">
                             <NavigationMenuItem>
                                 <Link href="/" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
                                         Home
                                     </NavigationMenuLink>
                                 </Link>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <Link href="#about" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
                                         About
                                     </NavigationMenuLink>
                                 </Link>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
-                                <Link href="#experiences" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                <Link
+                                    href="#experiences"
+                                    legacyBehavior
+                                    passHref
+                                >
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
                                         Experiences
                                     </NavigationMenuLink>
                                 </Link>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <Link href="#projects" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
                                         Projects
                                     </NavigationMenuLink>
                                 </Link>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <Link href="#skills" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
                                         Skills
                                     </NavigationMenuLink>
                                 </Link>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <Link href="#contact" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
                                         Contact
                                     </NavigationMenuLink>
                                 </Link>
@@ -112,5 +149,5 @@ export function NavigationBar() {
                 </SheetContent>
             </Sheet>
         </>
-    )
+    );
 }
